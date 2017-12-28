faf652e88f693349fb000a23c5b9389df7bda286
Luis Santos <luis@paiva.eu>
2017-12-28 16:03:23 +0000

    trimming down config file; updated create_titles_txt.py to use the new file format

--- question-generation/create_titles_txt.py
+++ question-generation/create_titles_txt.py
@@ -7,15 +7,18 @@ import sys
 with open('config.yml', 'r') as f:
     config = yaml.load(f)
 
+region = config['region']
 data_dir = config['dir_name']['data']
 posts_dir = config['dir_name']['posts']
-region = config['xml_extract']['region']
 
 db_file = os.path.join(posts_dir, 'Posts_' + region + '.db')
 
 if not os.path.isfile(db_file):
-    print(db_file, "doesn't exist")
-    sys.exit('Exiting')
+    sys.exit(db_file + " doesn't exist")
+
+#create data dir if it doesn't exist
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
 
 #create connection
 conn = sqlite3.connect(db_file)
@@ -30,11 +33,10 @@ for row in select_iter:
 #remove extra \n at the end
 titles_text = titles_text[:-2]
 
-if not os.path.exists(data_dir):
-    os.makedirs(data_dir)
-
 titles_file = os.path.join(data_dir, 'titles_' + region + '.txt')
 with open(titles_file, 'w', encoding='utf-8') as f:
     f.write(titles_text)
+
+print ('Created', titles_file)
 
 conn.close()
