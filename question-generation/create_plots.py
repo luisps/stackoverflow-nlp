38c890e815d572bf3919b977bc9a9665fd7db30a
Luis Santos <luis@paiva.eu>
2018-01-01 21:57:30 +0000

    plotting number of answers and active users in addition to number of questions; the plot for the popular tags through time became an area chart instead of a line chart

--- question-generation/create_plots.py
+++ question-generation/create_plots.py
@@ -32,40 +32,79 @@ conn = sqlite3.connect(db_file)
 cur = conn.cursor()
 
 
-#plot number of questions through time
+#plot number of questions, answers and active users through time
+
+#find date range(the date for the very first post and for the most recent one)
+date_range_query = '''
+SELECT MIN(CreationDate) AS startDate, MAX(CreationDate) AS endDate
+FROM Questions
+'''
+select_iter = cur.execute(date_range_query)
+
+#variables used by this and other timeseries plots
+start_date, end_date = select_iter.fetchone()
+last_year, last_month = int(end_date[:4]), int(end_date[5:7])
+
+df_index = pd.date_range(start_date, end_date, freq='D')
+df = pd.DataFrame(0, index=df_index, columns=['Questions', 'Answers', 'Active Users'])
+
 questions_per_day_query = '''
-SELECT CreationDate, count(Id)
-FROM Posts
+SELECT CreationDate, COUNT(Id)
+FROM Questions
 GROUP BY CreationDate
 '''
 select_iter = cur.execute(questions_per_day_query)
 
-date = []
-num_questions = []
 for row in select_iter:
-    date.append(row[0])
-    num_questions.append(row[1])
+    date, num_questions = row[0], row[1]
+    df.loc[date, 'Questions'] = num_questions
 
-s = pd.Series(num_questions, index=pd.to_datetime(date))
+answers_per_day_query = '''
+SELECT CreationDate, COUNT(Id)
+FROM Answers
+WHERE CreationDate >= "{}"
+GROUP BY CreationDate
+'''
+select_iter = cur.execute(answers_per_day_query.format(start_date))
 
-#variables used by this and other timeseries plots
-start_date, end_date = date[0], date[-1]
-last_year, last_month = int(end_date[:4]), int(end_date[5:7])
+for row in select_iter:
+    date, num_answers = row[0], row[1]
+    df.loc[date, 'Answers'] = num_answers
+
+active_users_per_day_query = '''
+SELECT CreationDate, SUM(activeUsers)
+FROM (
+SELECT CreationDate, COUNT(DISTINCT UserId) AS activeUsers
+FROM Questions
+GROUP BY CreationDate
+UNION ALL
+SELECT CreationDate, COUNT(DISTINCT UserId) AS activeUsers
+FROM Answers
+WHERE CreationDate >= "{}"
+GROUP BY CreationDate
+)
+GROUP BY CreationDate
+'''
+select_iter = cur.execute(active_users_per_day_query.format(start_date))
+
+for row in select_iter:
+    date, num_active_users = row[0], row[1]
+    df.loc[date, 'Active Users'] = num_active_users
 
 #remove last month as it is not complete and so it can not be used
 #when aggregating data in months
-s = s[(s.index.year != last_year) | (s.index.month != last_month)]
+df = df[(df.index.year != last_year) | (df.index.month != last_month)]
 
-s.resample('M').sum().plot(title='Number of questions')
+df.resample('M').mean().plot(title='StackOverflow evolution through time')
 
-plot_file = os.path.join(images_dir, 'timeseries_num_questions_{}.png'.format(region))
+plot_file = os.path.join(images_dir, 'stackoverflow_evolution_{}.png'.format(region))
 plt.savefig(plot_file)
 plt.close()
 print ('Created', plot_file)
 
 
 #plot number of tags per question
-select_iter = cur.execute('SELECT tags FROM Posts')
+select_iter = cur.execute('SELECT Tags FROM Questions')
 
 tag_count = defaultdict(int)
 num_tags = defaultdict(int)
@@ -93,16 +132,16 @@ print ('Created', plot_file)
 
 
 #plot most popular tags
-num_popular_tags = 15
+num_popular_tags = 10
 sorted_tag_freq = sorted(tag_count, key=tag_count.get)
 
 popular_tags = sorted_tag_freq[-num_popular_tags:][::-1]
 popular_tags_count = [tag_count[tag] for tag in popular_tags]
 
 s = pd.Series(popular_tags_count, index=popular_tags)
-s.plot.bar(title='Most popular {} tags'.format(num_popular_tags))
+s.plot.bar(title='Number of questions for popular tags')
 
-plot_file = os.path.join(images_dir, 'popular_tags_{}.png'.format(region))
+plot_file = os.path.join(images_dir, 'popular_tags_total_{}.png'.format(region))
 plt.tight_layout()
 plt.savefig(plot_file)
 plt.close()
@@ -111,15 +150,11 @@ print ('Created', plot_file)
 
 #plot number of questions through time for most popular tags
 tag_questions_per_day_query = '''
-SELECT CreationDate, count(Id)
-FROM Posts
+SELECT CreationDate, COUNT(Id)
+FROM Questions
 WHERE tags like '%<{}>%'
 GROUP BY CreationDate
 '''
-
-#reduce number of popular tags to avoid information overload on the plots
-num_popular_tags = 5
-popular_tags = popular_tags[:num_popular_tags]
 
 df_index = pd.date_range(start_date, end_date, freq='D')
 df = pd.DataFrame(0, index=df_index, columns=popular_tags)
@@ -135,9 +170,9 @@ for tag in popular_tags:
 #remove last month as it is not complete
 df = df[(df.index.year != last_year) | (df.index.month != last_month)]
 
-df.resample('M').sum().plot(title='Number of questions for popular tags')
+df.resample('M').mean().plot.area(title='Popular tags evolution through time')
 
-plot_file = os.path.join(images_dir, 'timeseries_popular_tags_{}.png'.format(region))
+plot_file = os.path.join(images_dir, 'popular_tags_evolution_{}.png'.format(region))
 plt.savefig(plot_file)
 plt.close()
 print ('Created', plot_file)
